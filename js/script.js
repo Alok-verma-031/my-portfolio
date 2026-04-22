7dd59d38a27a474f1e98a8609b8e5bf037828a37
Alok verma 031 <alokverma9569@gmail.com>
2026-04-22 22:08:12 +0530

    modify theme

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
+// nav scroll
 const nav = document.getElementById("nav");
-console.log(nav);
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > 100) {
@@ -15,3 +15,43 @@ window.addEventListener("scroll", () => {
         nav.style.backgroundColor = "";
     }
 });
+// for footer change year
+const currYear=document.getElementById("year");
+let date= new Date();
+let currentYear=date.getFullYear();
+currYear.innerHTML=currentYear;
+
+// text type
+const roles = [
+  "Java Developer",
+  "Full Stack Developer",
+  "Problem Solver"
+];
+
+const element = document.getElementById("typing");
+
+let roleIndex = 0;
+let charIndex = 0;
+
+function typeText() {
+  if (charIndex < roles[roleIndex].length) {
+    element.textContent += roles[roleIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(typeText, 100);
+  } else {
+    setTimeout(deleteText, 1500);
+  }
+}
+
+function deleteText() {
+  if (charIndex > 0) {
+    element.textContent = roles[roleIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(deleteText, 50);
+  } else {
+    roleIndex = (roleIndex + 1) % roles.length;
+    setTimeout(typeText, 500);
+  }
+}
+
+typeText();
